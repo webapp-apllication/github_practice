5eda4800ebe28eee866d7587eef7778625d3cf6f
HarikrishnaPydi <haridevops888@gmail.com>
2025-04-17 17:40:07 +0530

    Update op1.sh

---
+++ op1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "arthemetical operations"
+
+echo "enter a value"
+read a
+echo "enter b value"
+read b
+
+echo "sum$((a+b))"
+echo "sub((a-b))"
+echo "mul$((a*b))"
+echo "div$((a/b))"
